6311b9f7300decb53ebffdd79ca4a85d3edecad0
D.H. Tuhin <128525189+delowerhossentuhin@users.noreply.github.com>
2024-10-27 00:22:43 +0600

    Update 2_GraphTraversal_BFS(Queue).cpp

--- 2_GraphTraversal_BFS(Queue).cpp
+++ 2_GraphTraversal_BFS(Queue).cpp
@@ -29,10 +29,12 @@ vector<int>bfsOfGraph(int v,int st, vector<int>adj[])
 int main()
 {
     ////////File I/O Setup////////
+     /*
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
+    */
     ///////////////////////////////
     int n,m;
     //n is number of nodes and m is number of Edges
@@ -61,59 +63,4 @@ int main()
 7 8
 6 7
 6 9
-
-
-
-////////////
-code
-#include<iostream>
-#include<vector>
-#include<queue>
-using namespace std;
-vector<int>bfsOfGraph(int v,vector<int>adj[])
-{
-    int visited[v]={0};
-    visited[1]=1;
-    queue<int>q;
-    q.push(1);
-    vector<int>bfs;
-    while(!q.empty())
-    {
-        int node=q.front();
-        q.pop();
-        bfs.push_back(node);
-        for(auto it: adj[node])
-        {
-            if(!visited[it])
-            {
-                visited[it]=1;
-                q.push(it);
-            }
-        }
-    }
-    return bfs;
-
-}
-int main()
-{
-    ////////File I/O Setup////////
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-    ////////////////////////////
-    int n,m;
-    cin>>n>>m;
-    vector<int>adj[n+1];
-    for(int i=0;i<m;i++)
-    {
-        int u,v;
-        cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    vector<int>bfs=bfsOfGraph(n,adj);
-    for(int it:bfs)cout<<it<<" ";
-
-}
 */
