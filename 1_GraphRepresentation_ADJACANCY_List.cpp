7fb5282f1a91acd1b20affeb4e4658ce9f5bb51e
D.H. Tuhin <128525189+delowerhossentuhin@users.noreply.github.com>
2024-10-27 00:21:33 +0600

    Update 1_GraphRepresentation_ADJACANCY_List.cpp

--- 1_GraphRepresentation_ADJACANCY_List.cpp
+++ 1_GraphRepresentation_ADJACANCY_List.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 int main()
 {
-    ////////File I/O Setup////////
+     /*
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
-    ////////////////////////////
+    */
     int n,m;
     cin>>n>>m;
     vector<int>adj[n+1];
